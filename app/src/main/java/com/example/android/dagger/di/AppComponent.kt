da74bb177fda26fdd4ea953d5d8067e925af1733
Yusuke Suzuki <cs16047@s.inf.shizuoka.ac.jp>
2019-12-25 12:06:09 +0900

    update chapter7

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,11 +1,21 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
 // Definition of a Dagger component
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
-    // Classes that can be injected by this Component
+
+    // AppComponentのインスタンスを作成するファクトリー
+    @Component.Factory
+    interface Factory {
+        // @BindsInstanceを使用すると、渡されたコンテキストはグラフで利用可能に
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    // RegistrationActivityがInjectを要求している
     fun inject(activity: RegistrationActivity)
 }
